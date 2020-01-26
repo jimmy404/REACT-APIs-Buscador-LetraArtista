674920ed1c91a7aae22400f20e3c03e251a2afe5
Jimmy <bruno_pier@hotmail.com>
2020-01-26 01:09:40 -0300

    Adding fix to show lyrics - Cancion.js

--- src/components/Cancion.js
+++ src/components/Cancion.js
@@ -1,11 +1,16 @@
 import React, { Fragment } from 'react';
 
-const Cancion = ({letra}) => (
-    <Fragment>
-        <h2>Letra Cancion</h2>
-            <p className="letra">{letra}</p>
-    </Fragment>
+const Cancion = ({letra}) => {
+
+    if(letra.length === 0) return null;
+
+    return(
+            <Fragment>
+                <h2>Letra Cancion</h2>
+                    <p className="letra">{letra}</p>
+            </Fragment>
     );
+}
 
 
 export default Cancion;
